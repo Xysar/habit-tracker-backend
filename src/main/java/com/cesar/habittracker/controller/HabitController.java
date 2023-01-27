8963dd634bff1a5b4398bdc4c8f33bc9bb353126
Cesar Camacho <cesarcamacho760@gmail.com>
2023-01-26 18:17:17 -0800

    change cross origin

--- src/main/java/com/cesar/habittracker/controller/HabitController.java
+++ src/main/java/com/cesar/habittracker/controller/HabitController.java
@@ -10,7 +10,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@CrossOrigin("http://localhost:3000")
+@CrossOrigin("https://habit-tracker-frontend-production.up.railway.app/")
 public class HabitController {
 
     @Autowired
